1a5b1e3d8390befa6afcc11abd033b8a988f10f8
iolite312 <68647023+iolite312@users.noreply.github.com>
2024-09-23 15:59:23 +0200

    Renamed/Removed files

--- src/main/java/me/leon/theater/TheaterApp.java
+++ src/main/java/me/leon/theater/TheaterApp.java
@@ -4,13 +4,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import me.leon.theater.models.Database;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class TheaterApp extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Database database = new Database();
+        FXMLLoader fxmlLoader = new FXMLLoader(TheaterApp.class.getResource("login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
