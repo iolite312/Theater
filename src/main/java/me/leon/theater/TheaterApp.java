d029a682082b460f399ba515a21e6f66c0cfd9d4
iolite312 <68647023+iolite312@users.noreply.github.com>
2024-09-24 20:28:33 +0200

    overridden the stop event to do custom database saving to file

--- src/main/java/me/leon/theater/TheaterApp.java
+++ src/main/java/me/leon/theater/TheaterApp.java
@@ -8,15 +8,23 @@ import me.leon.theater.data.Database;
 import java.io.IOException;
 
 public class TheaterApp extends Application {
+    Database database = new Database();
 
     @Override
     public void start(Stage stage) throws IOException {
-        Database database = new Database();
         SceneController sceneController = new SceneController(database, stage);
         sceneController.setRootScene("login");
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    @Override
+    public void stop() {
+        System.out.println(database.getLoggedInUser().getUserName());
+        database.logOut();
+        System.out.println("Stage is closing");
+        System.out.println(database.getLoggedInUser());
     }
 }
