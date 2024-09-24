3c9a4b244cb45b0684a6ec93a7e741f5de49be4c
iolite312 <68647023+iolite312@users.noreply.github.com>
2024-09-24 20:29:47 +0200

    Refactored the remove duplicate code

--- src/main/java/me/leon/theater/controllers/SceneController.java
+++ src/main/java/me/leon/theater/controllers/SceneController.java
@@ -31,27 +31,17 @@ public class SceneController {
     }
 
     public void setRootScene(String name) throws IOException {
-        FXMLLoader loader = new FXMLLoader(TheaterApp.class.getResource(scenes.get(name)));
-        if ("login".equals(name)) {
-            loader.setController(new LoginController(database, this));
-        } else if ("home".equals(name)) {
-            loader.setController(new HomeViewController(database, this));
-        }
-
-        root = loader.load();
-        if (root == null) {
-            throw new IllegalArgumentException("Scene not found");
-        }
-        Scene scene = new Scene(root);
-
-        stage.setScene(scene);
+        SceneLoader(name);
         stage.show();
     }
 
     public void setRootScene(String name, ActionEvent actionEvent) throws IOException {
         stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        FXMLLoader loader = new FXMLLoader(TheaterApp.class.getResource(scenes.get(name)));
+        SceneLoader(name);
+    }
 
+    private void SceneLoader(String name) throws IOException {
+        FXMLLoader loader = new FXMLLoader(TheaterApp.class.getResource(scenes.get(name)));
         if ("login".equals(name)) {
             loader.setController(new LoginController(database, this));
         } else if ("home".equals(name)) {
