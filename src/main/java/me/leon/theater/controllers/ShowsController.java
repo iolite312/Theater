1456f640cc0ee761fc819e92ea823e5945b0b1c1
iolite312 <68647023+iolite312@users.noreply.github.com>
2024-10-01 11:30:36 +0200

    Loading shows into the view

--- src/main/java/me/leon/theater/controllers/ShowsController.java
+++ src/main/java/me/leon/theater/controllers/ShowsController.java
@@ -1,13 +1,29 @@
 package me.leon.theater.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
 import me.leon.theater.data.Database;
+import me.leon.theater.models.Shows;
 
 public class ShowsController {
     private Database database;
 
     private SceneController sceneController;
+
+    private ObservableList<Shows> shows;
+
+    @FXML
+    private TableView showingsTableView;
+
     public ShowsController(Database database, SceneController sceneController) {
         this.database = database;
         this.sceneController = sceneController;
+    }
+    @FXML
+    public void initialize() {
+        shows = FXCollections.observableArrayList(database.getShows());
+        showingsTableView.setItems(shows);
     }
 }
