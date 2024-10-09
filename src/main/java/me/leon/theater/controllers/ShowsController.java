c0452051765adb0c6c6b3733ae1bdb3daca9fd17
iolite312 <68647023+iolite312@users.noreply.github.com>
2024-10-09 15:35:58 +0200

    Beginning of CRUD for shows

--- src/main/java/me/leon/theater/controllers/ShowsController.java
+++ src/main/java/me/leon/theater/controllers/ShowsController.java
@@ -2,8 +2,10 @@ package me.leon.theater.controllers;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
+import javafx.scene.text.Text;
 import me.leon.theater.data.Database;
 import me.leon.theater.models.Shows;
 
@@ -16,6 +18,8 @@ public class ShowsController {
 
     @FXML
     private TableView showingsTableView;
+    @FXML
+    private Text errorMessage;
 
     public ShowsController(Database database, SceneController sceneController) {
         this.database = database;
@@ -25,5 +29,22 @@ public class ShowsController {
     public void initialize() {
         shows = FXCollections.observableArrayList(database.getShows());
         showingsTableView.setItems(shows);
+    }
+
+    public void addShowing(ActionEvent actionEvent) {
+        sceneController.StartDialog("Add Showing", "showsDialog-view.fxml", new ShowsDialogController(database));
+    }
+
+    public void editShowing(ActionEvent actionEvent) {
+
+    }
+
+    public void deleteShowing(ActionEvent actionEvent) {
+        ObservableList<Shows> selectedShow = showingsTableView.getSelectionModel().getSelectedItems();
+        if (selectedShow.getFirst().getRoom().getSeatsLeft() == selectedShow.getFirst().getRoom().getTotalSeats()) {
+            shows.removeAll(selectedShow);
+            return;
+        }
+        errorMessage.setText("This showing has already sold tickets");
     }
 }
