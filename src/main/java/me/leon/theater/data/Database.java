9c5bbdcf4dbd1d9a1362bc06b3634415646b2fd8
iolite312 <68647023+iolite312@users.noreply.github.com>
2024-10-09 17:10:36 +0200

    Changed the db to get rid of errors

--- src/main/java/me/leon/theater/data/Database.java
+++ src/main/java/me/leon/theater/data/Database.java
@@ -9,7 +9,6 @@ import java.util.List;
 public class Database {
     private List<User> users = new ArrayList<>();
     private List<Shows> shows = new ArrayList<>();
-    private List<Ticket> tickets = new ArrayList<>();
     private List<Room> rooms = new ArrayList<>();
     private User loggedInUser;
 
@@ -18,18 +17,21 @@ public class Database {
         seedDatabase();
     }
     private void seedDatabase() {
+        createRooms();
         users.add(new User(1, "manager", "1234", Role.MANAGER));
         users.add(new User(2, "sales", "1234", Role.SALES));
-        shows.add(new Shows("Rebel Moon - Part Two: The ScarGiver", LocalDateTime.of(2024, 10, 4, 14, 0), LocalDateTime.of(2024, 10, 4, 16, 30), new Room("Room 1", 72, 70)));
-        shows.add(new Shows("Rebel Moon - Part Two: The ScarGiver", LocalDateTime.of(2024, 10, 5, 20, 0), LocalDateTime.of(2024, 10, 4, 22, 30), ));
         List<Seat> seats = new ArrayList<>();
         seats.add(new Seat(4,4));
         seats.add(new Seat(4,5));
-        tickets.add(new Ticket(seats, "Mark de Haan", shows.get(0), LocalDateTime.of(2024, 10, 2, 16, 35)));
-        tickets.add(new Ticket(seats, "Erwin de Vries", shows.get(1), LocalDateTime.of(2024, 10, 2, 17, 2)));
+        List<Ticket> tickets1 = new ArrayList<>();
+        List<Ticket> tickets2 = new ArrayList<>();
+        tickets1.add(new Ticket(seats, "Mark de Haan", LocalDateTime.of(2024, 10, 2, 16, 35), "Rebel Moon - Part Two: The ScarGiver"));
+        tickets2.add(new Ticket(seats, "Erwin de Vries", LocalDateTime.of(2024, 10, 2, 17, 2), "Rebel Moon - Part Two: The ScarGiver"));
+        shows.add(new Shows("Rebel Moon - Part Two: The ScarGiver", LocalDateTime.of(2024, 10, 4, 14, 0), LocalDateTime.of(2024, 10, 4, 16, 30), rooms.getFirst(), tickets1));
+        shows.add(new Shows("Rebel Moon - Part Two: The ScarGiver", LocalDateTime.of(2024, 10, 5, 20, 0), LocalDateTime.of(2024, 10, 4, 22, 30), rooms.getFirst(), tickets2));
     }
     private void createRooms() {
-        rooms.add(new Room("Room 1", 72, 70));
+        rooms.add(new Room("Room 1", 72));
     }
 
     public List<User> getUsers() {
@@ -38,10 +40,9 @@ public class Database {
     public List<Shows> getShows() {
         return shows;
     }
-    public List<Ticket> getTickets() {
-        return tickets;
+    public List<Room> getRooms() {
+        return rooms;
     }
-    public List
     public User getLoggedInUser() {
         return loggedInUser;
     }
