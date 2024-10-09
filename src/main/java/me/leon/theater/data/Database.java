e1d9bc5efd11e2830505a91116eef39ab08e15a1
iolite312 <68647023+iolite312@users.noreply.github.com>
2024-10-09 15:39:06 +0200

    Database is broken atm

--- src/main/java/me/leon/theater/data/Database.java
+++ src/main/java/me/leon/theater/data/Database.java
@@ -10,6 +10,7 @@ public class Database {
     private List<User> users = new ArrayList<>();
     private List<Shows> shows = new ArrayList<>();
     private List<Ticket> tickets = new ArrayList<>();
+    private List<Room> rooms = new ArrayList<>();
     private User loggedInUser;
 
     public Database() {
@@ -19,13 +20,16 @@ public class Database {
     private void seedDatabase() {
         users.add(new User(1, "manager", "1234", Role.MANAGER));
         users.add(new User(2, "sales", "1234", Role.SALES));
-        shows.add(new Shows(1, "Rebel Moon - Part Two: The ScarGiver", LocalDateTime.of(2024, 10, 4, 14, 0), LocalDateTime.of(2024, 10, 4, 16, 30), new Room("Room 1", 72, 70)));
-        shows.add(new Shows(2, "Rebel Moon - Part Two: The ScarGiver", LocalDateTime.of(2024, 10, 5, 20, 0), LocalDateTime.of(2024, 10, 4, 22, 30), new Room("Room 1", 72, 70)));
+        shows.add(new Shows("Rebel Moon - Part Two: The ScarGiver", LocalDateTime.of(2024, 10, 4, 14, 0), LocalDateTime.of(2024, 10, 4, 16, 30), new Room("Room 1", 72, 70)));
+        shows.add(new Shows("Rebel Moon - Part Two: The ScarGiver", LocalDateTime.of(2024, 10, 5, 20, 0), LocalDateTime.of(2024, 10, 4, 22, 30), ));
         List<Seat> seats = new ArrayList<>();
         seats.add(new Seat(4,4));
         seats.add(new Seat(4,5));
         tickets.add(new Ticket(seats, "Mark de Haan", shows.get(0), LocalDateTime.of(2024, 10, 2, 16, 35)));
         tickets.add(new Ticket(seats, "Erwin de Vries", shows.get(1), LocalDateTime.of(2024, 10, 2, 17, 2)));
+    }
+    private void createRooms() {
+        rooms.add(new Room("Room 1", 72, 70));
     }
 
     public List<User> getUsers() {
@@ -37,6 +41,7 @@ public class Database {
     public List<Ticket> getTickets() {
         return tickets;
     }
+    public List
     public User getLoggedInUser() {
         return loggedInUser;
     }
