3d2e4c168de9794504e59a0a4b3fb773071210bd
iolite312 <68647023+iolite312@users.noreply.github.com>
2024-10-11 17:11:36 +0200

    Room size

--- src/main/java/me/leon/theater/models/Room.java
+++ src/main/java/me/leon/theater/models/Room.java
@@ -7,10 +7,14 @@ public class Room implements Serializable
 {
     private String roomName;
     private int totalSeats;
+    private int rows;
+    private int columns;
 
-    public Room(String roomName, int totalSeats) {
+    public Room(String roomName, int totalSeats, int rows, int columns) {
         this.roomName = roomName;
         this.totalSeats = totalSeats;
+        this.rows = rows;
+        this.columns = columns;
     }
 
     public String getRoomName() {
@@ -19,6 +23,12 @@ public class Room implements Serializable
 
     public int getTotalSeats() {
         return totalSeats;
+    }
+    public int getRows() {
+        return rows;
+    }
+    public int getColumns() {
+        return columns;
     }
 
     @Override
