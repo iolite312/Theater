b83f85a794247ab9690f0407d834aa8ce5d22657
iolite312 <68647023+iolite312@users.noreply.github.com>
2024-10-11 17:11:24 +0200

    Sales view + controller

--- src/main/java/me/leon/theater/controllers/SalesController.java
+++ src/main/java/me/leon/theater/controllers/SalesController.java
@@ -1,13 +1,51 @@
 package me.leon.theater.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import me.leon.theater.data.Database;
+import me.leon.theater.models.Shows;
+
+import java.util.Comparator;
 
 public class SalesController {
     private Database database;
 
     private SceneController sceneController;
-    public SalesController(Database database, SceneController sceneController) {
+
+    private ObservableList<Shows> shows;
+
+    private VBox vbox;
+
+    @FXML
+    private TableView<Shows> showingsTableView;
+    @FXML
+    private Text errorMessage;
+
+    public SalesController(Database database, SceneController sceneController, VBox vbox) {
         this.database = database;
         this.sceneController = sceneController;
+        this.vbox = vbox;
+    }
+    @FXML
+    public void initialize() {
+        shows = FXCollections.observableArrayList(database.getShows());
+
+        showingsTableView.setItems(shows);
+    }
+
+    public void sellTickets(ActionEvent actionEvent) {
+        errorMessage.setText("");
+        Shows show = showingsTableView.getSelectionModel().getSelectedItem();
+        if (show == null) {
+            errorMessage.setText("No show selected"); return;
+        }
+        TicketController ticketController = new TicketController(database, sceneController, show, vbox);
+
+        sceneController.loadSubScenes("ticket-view.fxml", ticketController, vbox);
     }
 }
